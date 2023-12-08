5ddd094c6bbccc3a218ee17ec707a12e0c269bea
Carlos Eduardo <127683028+krarlos@users.noreply.github.com>
2023-12-08 21:41:33 +0000

    todo list react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 //import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
+
+
 	return (
-		<div className="text-center">
-			
-		</div>
+		<body>
+			<div className="body">
+				<h1>My tareas</h1>
+				<div className="caja">
+					<ul>
+						<li>
+							<input
+								type="text"
+								onChange={(event) => setInputValue(event.target.value)}
+								value={inputValue}
+								onKeyPress={(event) => {
+									if (event.key === "Enter" && event.target.value !=="") {
+										setTodos(todos.concat(inputValue))
+										setInputValue("");
+									}
+									{/* linea de arriba evita que se envie un campo en blanco, hace reset al placeholder con "" y envia la tarea */}
+								}}
+
+								placeholder="What you need to do?">
+
+							</input>
+						</li>
+						
+						{todos.map((item, index) => (
+							<li>
+
+								{item}{""}
+								{/* evento para eliminar las tareas */}
+								<i
+									class="fas fa-trash-alt"
+									
+									onClick={() =>
+										setTodos(
+											todos.filter(
+												(t, currentIndex) =>
+													index != currentIndex
+											)
+										)
+									}></i>
+							</li>
+						))}
+					</ul>
+					<div className="task">{todos.length} task</div>
+				</div>
+			</div>
+			<script src="https://kit.fontawesome.com/90d12af69a.js" crossorigin="anonymous"></script>
+		</body>
 	);
 };
 
 export default Home;
+
