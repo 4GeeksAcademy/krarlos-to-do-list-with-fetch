59125ad22dae16c461863f9e061fa6064f2a4b32
Carlos Eduardo Ramirez Artigas <c_ramirez@Carloss-MacBook-Air.local>
2023-12-08 23:12:59 -0300

    prueba push remoto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,61 +5,62 @@ import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
+  {
+    /* comentario de push remote prueba */
+  }
 
+  return (
+    <body>
+      <div className="body">
+        <h1>My tareas</h1>
+        <div className="caja">
+          <ul>
+            <li>
+              <input
+                type="text"
+                onChange={(event) => setInputValue(event.target.value)}
+                value={inputValue}
+                onKeyPress={(event) => {
+                  if (event.key === "Enter" && event.target.value !== "") {
+                    setTodos(todos.concat(inputValue));
+                    setInputValue("");
+                  }
+                  {
+                    /* linea de arriba evita que se envie un campo en blanco, hace reset al placeholder con "" y envia la tarea */
+                  }
+                }}
+                placeholder="What you need to do?"
+              ></input>
+            </li>
 
-	return (
-		<body>
-			<div className="body">
-				<h1>My tareas</h1>
-				<div className="caja">
-					<ul>
-						<li>
-							<input
-								type="text"
-								onChange={(event) => setInputValue(event.target.value)}
-								value={inputValue}
-								onKeyPress={(event) => {
-									if (event.key === "Enter" && event.target.value !=="") {
-										setTodos(todos.concat(inputValue))
-										setInputValue("");
-									}
-									{/* linea de arriba evita que se envie un campo en blanco, hace reset al placeholder con "" y envia la tarea */}
-								}}
-
-								placeholder="What you need to do?">
-
-							</input>
-						</li>
-						
-						{todos.map((item, index) => (
-							<li>
-
-								{item}{""}
-								{/* evento para eliminar las tareas */}
-								<i
-									class="fas fa-trash-alt"
-									
-									onClick={() =>
-										setTodos(
-											todos.filter(
-												(t, currentIndex) =>
-													index != currentIndex
-											)
-										)
-									}></i>
-							</li>
-						))}
-					</ul>
-					<div className="task">{todos.length} task</div>
-				</div>
-			</div>
-			<script src="https://kit.fontawesome.com/90d12af69a.js" crossorigin="anonymous"></script>
-		</body>
-	);
+            {todos.map((item, index) => (
+              <li>
+                {item}
+                {""}
+                {/* evento para eliminar las tareas */}
+                <i
+                  class="fas fa-trash-alt"
+                  onClick={() =>
+                    setTodos(
+                      todos.filter((t, currentIndex) => index != currentIndex)
+                    )
+                  }
+                ></i>
+              </li>
+            ))}
+          </ul>
+          <div className="task">{todos.length} task</div>
+        </div>
+      </div>
+      <script
+        src="https://kit.fontawesome.com/90d12af69a.js"
+        crossorigin="anonymous"
+      ></script>
+    </body>
+  );
 };
 
 export default Home;
-
